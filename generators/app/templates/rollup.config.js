a3c5bd2e33083578544992c5a6874fcc09c874ad
stan1812 <2314980309@qq.com>
2018-11-10 00:39:55 +0800

    almost done

--- generators/app/templates/rollup.config.js
+++ generators/app/templates/rollup.config.js
@@ -1,16 +1,28 @@
-import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
+import eslint from 'rollup-plugin-eslint';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+// import replace from 'rollup-plugin-replace';
+// import uglify from 'rollup-plugin-uglify';
 
 export default {
-  input: 'src/main.js',
+  input: 'src/index.js',
   output: {
     file: 'bundle.js',
-    format: 'cjs'
+    format: 'cjs',
   },
   plugins: [
-    resolve(),
+    resolve({
+      jsnext: true,
+      main: true,
+      browser: true,
+    }),
+    commonjs(),
+    eslint({
+      exclude: ['src/styles/**'],
+    }),
     babel({
-      exclude: 'node_modules/**'
-    })
-  ]
+      exclude: 'node_modules/**',
+    }),
+  ],
 };
